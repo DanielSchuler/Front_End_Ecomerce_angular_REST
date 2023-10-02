1a2101fe9f44af1bb2e09de492237272d52acfef
DanielSchuler <daniel.andreas87@gmail.com>
2023-10-02 11:19:33 +0200

    prueba subida ignorada

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -1,1 +1,14 @@
-
+export const environment ={
+    production: false,
+    name:'dev',
+    firebase:{
+        config : {
+            apiKey: "AIzaSyDZhyFPVrALPnY_r_Okso4SWFJwYPGd1To",
+            authDomain: "tienda-41fb0.firebaseapp.com",
+            projectId: "tienda-41fb0",
+            storageBucket: "tienda-41fb0.appspot.com",
+            messagingSenderId: "1097494614387",
+            appId: "1:1097494614387:web:7a03f22aa98d3005117506"
+          }
+    }
+}
